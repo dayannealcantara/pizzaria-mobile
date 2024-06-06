ea4bf344d016b9c17a8efd5af9451ea08012a0f0
dayannealcantara <dayannealcantara14@gmail.com>
2024-06-06 12:01:41 -0300

    feat: configuring loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { ActivityIndicator, View } from "react-native"
 import AppRoutes from "./app.routes"
 import AuthRoutes from "./auth.routes"
 
@@ -5,6 +6,21 @@ import AuthRoutes from "./auth.routes"
 function Routes() {
   const isAuthenticated = false
   const loading = false
+
+  if(loading){
+    return(
+      <View 
+        style={{
+          flex:1,
+          backgroundColor:'#f5f7fb',
+          justifyContent:'center',
+          alignItems:'center'
+        }}
+      >
+        <ActivityIndicator size={60} color="#1d1d2e"/>
+      </View>
+    )
+  }
 
   return (
     isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
